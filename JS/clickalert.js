46a9a7abd01f399ae10b122ccdfad1a07a7346f2
AxJMP <85538806+AxJMP@users.noreply.github.com>
2021-06-11 19:56:30 -0500

    Update clickalert.js

--- JS/clickalert.js
+++ JS/clickalert.js
@@ -1,26 +1,26 @@
 /*MORALES PEREZ AXEL JONATHNA*/
 $(document).ready(function(){
-	$("A").click(function(evento){ 
+	$("#A").click(function(evento){ 
 	alert("REFRESCAR MENU");
 	});
 	
-	$("a").click(function(evento){ 
+	$("#a").click(function(evento){ 
 	alert("ESTAS POR INGRESAR NUESTRAS ESPECALEIDADES DE LA ESCUELA");
 	});
 	
-	$("e").click(function(evento){ 
+	$("#e").click(function(evento){ 
 	alert("ESTAS POR INGRESAR A LOS PROYECTOS ESCOLARES");
 	});
 	
-	$("i").click(function(evento){ 
+	$("#i").click(function(evento){ 
 	alert("ESTAS POR INGRESAR EN LA SECCION DE CONOCENOS, !CONOCE EL PLANTEL CBTIS N.243¡");
 	});
 	
-	$("o").click(function(evento){ 
+	$("#o").click(function(evento){ 
 	alert("ESTAS POR INGRESAR A CONTRUYETE, PROGRAMA DE AUTOCONOCMENTO PARA EL ALUMNO");
 	});
 	
-	$("u").click(function(evento){ 
+	$("#u").click(function(evento){ 
 	alert("ESTAS POR INGRESAR A BECAS BENITO JUAREZ";
 	});
 });
